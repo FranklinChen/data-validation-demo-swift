a8949ea7c8358dd7b743066c64503d2c1f811c52
Franklin Chen <franklinchen@franklinchen.com>
2015-03-30 18:51:29 -0400

    Initial Commit

---
+++ DataValidationDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DataValidationDemo
+//
+//  Created by Franklin Chen on 3/30/15.
+//  Copyright (c) 2015 Franklin Chen. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
